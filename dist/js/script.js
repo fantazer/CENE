f8a9697bbae7b2993d56d0f214a82445f9f86b93
FANTAZER <kuznecovs45@gmail.com>
2019-02-10 13:17:38 +0300

    add mobile version

--- dist/js/script.js
+++ dist/js/script.js
@@ -27,4 +27,40 @@ $(document).ready(function () {
 		$('.catalog').removeClass('catalog--active');
 		$('.catalog').eq(currentTab).addClass('catalog--active');
 	});
+
+	//mobile menu
+	//Фиксируем скрол
+	$('.head-toggle--open').click(function(){
+		$('body').css({
+			overflow: '',
+			position: '',
+			top: ''
+		})
+	});
+
+	$('.head-toggle').click(function(event){
+		event.stopPropagation();
+		$(this).toggleClass('head-toggle--open');
+		$('.slide-menu').toggleClass('slide-menu--open');
+		$('body').toggleClass('body-fix')
+	});
+
+	$('.slide-menu').on("click", function (event) {
+		event.stopPropagation();
+	});
+	var modalState = {
+		"isModalShow": false, //state show modal
+		"scrollPos": 0
+	};
+	$(document).on("click", function () {
+			$('.head-wrap').removeClass('head--up');
+			$('.head-toggle').removeClass('head-toggle--open');
+			$('.slide-menu').removeClass('slide-menu--open');
+			console.log(modalState.isModalShow);
+			if(modalState.isModalShow == false){
+				$('body').removeClass('body-fix')
+		}
+	});
+	//mobile menu===end
+
 });
